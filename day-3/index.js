ff3f783994c7908e4e871ef6147358f8672c1173
chand-Pc <jafri.nbj@gmail.com>
2023-01-01 00:20:44 +0530

    m pushing assignment

---
+++ day-3/index.js
@@ -0,0 +1,91 @@
+const arument = process.argv[2];
+const para1 = process.argv[3];
+const para2 = process.argv[4];
+
+const fs= require('fs');
+// if(arument=='f'){
+//     fs.writeFile(para1,para2,{encoding:'utf-8'},(err)=>{
+//     if(err){
+//         console.log(err);
+//     }
+//     console.log('finished');
+//     })
+// }
+// const data=fs.writeFile('test.txt',"/n this is new test file",{encoding:'utf-8'},(err)=>{
+//     if(err){
+//         console.log("error is",err);
+//     }
+    
+// });
+
+
+// fs.readFile('test.txt',{encoding:'utf-8'},(err,data1)=>{
+//     if(err){
+//         console.log(err);
+//     }
+// console.log(data1);
+    
+// })
+
+// const checkEven = require('is-even');
+// let d = checkEven(6);
+// // console.log(d);
+
+const dns = require('dns');
+// const fs= require('fs');
+// console.log(dns)
+// const getIp=(name)=>{
+//     dns.lookup(name,(err,address)=>{
+//         if(err){
+//             console.log(err);
+//         }
+//         console.log(name," is ",address)
+//     })
+// }
+
+
+// console.log(arument,para1,para2);
+if(arument=='a'){
+    getIp(para1);
+}
+
+
+// const fs = require('fs');
+// // fs.rm('test.txt',(err)=>{
+// // if(err){
+// //     console.log(err)
+// // }
+// // });
+
+// fs.writeFile('test.txt','content',(err)=>{
+// if(err){
+//     console.log(err)
+// }
+// })
+
+const readFile=(para1)=>{
+    fs.readFile(para1,{encoding:'utf-8'},(err,data)=>{
+if(err){
+    console.log(err);
+}
+console.log(data);
+    })
+}
+
+
+// to read file......
+if(arument=='s'){
+    readFile(para1);
+}
+
+const deletefile=(para1)=>{
+    fs.rm(para1,(err)=>{
+if(err){
+    console.log(err);
+}
+    })
+}
+
+if(arument=='rm'){
+    deletefile(para1);
+}
---
+++ day-5/index.js
@@ -0,0 +1,73 @@
+const expres = require('express');
+const fs = require('fs')
+const app = expres();
+app.use(expres.json())
+app.get('/',(req,res)=>{
+    res.send("hello word");
+});
+
+// --------for getting a data-------
+
+
+app.get('/getdata',(req,res)=>{
+const data = fs.readFileSync('db.json','utf-8');
+const d1= JSON.parse(data);
+res.send(d1.student);
+
+
+})
+
+// --------for adding a student details-----
+
+app.post('/addstudent',(req,res)=>{
+    const student = req.body;
+    const data = fs.readFileSync('db.json',"utf-8");
+    const parse_data = JSON.parse(data);
+    const student_data=parse_data.student
+    student_data.push(student);
+    fs.writeFileSync('db.json',JSON.stringify(parse_data,'utf-8'));
+    res.send("thank")
+    
+})
+
+// ---------------------for deleting a student details-----
+
+app.post('/deletestudent',(req,res)=>{
+    const id=req.body.id;
+    const data = fs.readFileSync('db.json','utf-8');
+    const d1 = JSON.parse(data);
+    let student = d1.student;
+   const da= student.filter((ele)=>{
+            return ele.id !=id; 
+    });
+    
+    
+    
+    d1.student=da;
+    fs.writeFileSync('db.json',JSON.stringify(d1,'utf-8'))
+    res.send('thanks')
+})
+
+// ----update data ---------
+
+
+app.post('/patch',(req,res)=>{
+    const id=req.body;
+    const data = fs.readFileSync('db.json','utf-8');
+    const parse_data = JSON.parse(data);
+    const student = parse_data.student;
+    student.forEach((ele)=>{
+            if(ele.id == id.id){
+                ele.name=id.name;
+                ele.age=id.age;
+            }
+    });
+    fs.writeFileSync('db.json',JSON.stringify(parse_data),'utf-8');
+    
+    
+    res.send('thanks')
+})
+
+app.listen(7000,()=>{
+
+})
